e35adf076ab803a4c7a204258625bba29f889193
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-16 01:01:42 +0200

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JokesComponent } from './components/jokes/jokes.component';
+import { JokeFormComponent } from './components/jokes/joke-form/joke-form.component';
+import { JokeListComponent } from './components/jokes/joke-list/joke-list.component';
+import { JokeCardComponent } from './components/jokes/joke-card/joke-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokesComponent,
+    JokeFormComponent,
+    JokeListComponent,
+    JokeCardComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
